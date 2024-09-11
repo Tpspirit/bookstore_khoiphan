2ca959130058c9c9c90f7a842801990d24175213
Phan Khoi <giakhoi2015@gmail.com>
2024-09-12 00:50:27 +0300

    Add Listpage + CRUD

--- src/main/java/fi/haagahelia/bookstore_khoiphan/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore_khoiphan/web/BookController.java
@@ -1,15 +1,42 @@
 package fi.haagahelia.bookstore_khoiphan.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fi.haagahelia.bookstore_khoiphan.domain.Book;
+import fi.haagahelia.bookstore_khoiphan.domain.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
+
+    @Autowired
+    private BookRepository repository;
     
-    @GetMapping("/index")
-    public String index(){
-        return "Hello this is my Bookstore";
+    @GetMapping({"/", "/booklist"})
+    public String booklist(Model model){
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/add")
+    public String add(Book book){
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        repository.deleteById(id);
+        return "redirect:../booklist";
     }
 }
